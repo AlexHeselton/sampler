e6eeedf37d3e776f6fbe13811888b5d9d81fa974
AlexHeselton <alex.heselton@gmail.com>
2017-03-16 09:56:56 -0700

    Three sample buttons

--- sketch1.js
+++ sketch1.js
@@ -1,13 +1,109 @@
 //Sampler for Taurin
 
+//rectangle variables
+var rx;
+var ry;
+var rw = 80;
+var rh = 60;
+
+//circle variables
+var cd;
+var cx;
+var cy;
+var cRadius = 40;
+
+var ed;
+var ex;
+var ey;
+var eRadius = 40;
+var startRadius = 40;
+var maxRadius = 60;
+var growthSpeed = 0.25;
+var grow = false;
+
+var one;
+var two;
+var three;
+
+function preload() {
+	one = loadSound("one.wav");
+}
+function preload() {
+	two = loadSound("two.wav");
+}
+function preload() {
+	three = loadSound("three.wav");
+}
+
+
 function setup() {
-
-
-  
+	createCanvas(windowWidth, windowHeight); 
+	ellipseMode(RADIUS);
+	rx = windowWidth/4;
+	ry = windowHeight/4;
+	cx = (windowWidth/4) * 3;
+	cy = windowHeight/4;
+	ex = windowWidth/2;
+	ey = (windowHeight / 4) * 3;
 }
 
 function draw() {
 
+	background(255);
+	//rectangle
+	if ((mouseX > rx) && (mouseX < rx + rw) && (mouseY > ry) && (mouseY < ry + rh) && (mouseIsPressed)) {
+		fill(255);
+		one.play();
+	}
+	else if ((mouseX > rx) && (mouseX < rx + rw) && (mouseY > ry) && (mouseY < ry + rh)) {
+		fill(125); 
+	}
+	else {
+		fill(0);
+	}
+	rect(rx, ry, rw, rh);
+
+	//Circle
+	cd = dist(mouseX, mouseY, cx, cy);
+	if ((cd < cRadius) && (mouseIsPressed)) {
+		fill(255);
+		two.play();
+	}
+	else if (cd < cRadius) {
+		fill(125);
+	}
+	else {
+		fill(0);
+	}
+	ellipse(cx, cy, cRadius);
+
+	//ellipse
+	// ed = dist(mouseX, mouseY, ex, ey);
+	// if ((ed < eRadius) && (mouseIsPressed)) {
+ //    	if (eRadius <= startRadius) {
+ //        	eRadius += growthSpeed;
+ //        	grow = true;
+ //    	}
+ //    	else if ((eRadius > startRadius) && (eRadius < maxRadius)) {
+ //        	if (grow == true) {
+ //          	eRadius += growthSpeed;
+ //        	}
+ //        }
+ //        else {
+ //          	eRadius -= growthSpeed;
+ //        }
+ //    }
+ //    else {
+ //    	if  (eRadius > startRadius) {
+ //    		eRadius -= growthSpeed;
+ //        	grow = false;	
+ //    	}
+ //    	else {
+ //    		eRadius = startRadius;
+ //    	}
+        
+ //    }
+ //    ellipse(ex, ey, eRadius);
  
 }
 
